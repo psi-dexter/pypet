6e103cb270838483402a517e78184b5beaadf696
psi_dexter <psi.dexter@gmail.com>
2014-11-17 08:38:24 +0300

    implement Car data stuctures and some methods

--- motor_net.go
+++ motor_net.go
@@ -3,6 +3,82 @@ package main
 // #include "pigpiod_if.h"
 import "C"
 import "time"
+const (
+	frontLeft_pin int = 17
+	frontRight_pin int = 22
+	rearLeft_pin int = 27
+	rearRight_pin int = 23
+
+	leftPWM_pin int = 18
+	rightPWM_pin int = 25
+)
+type Car struct{
+	frontLeft_pin, frontRight_pin, rearLeft_pin, rearRight_pin int
+	leftPWM_pin, rightPWM_pin int
+	direction string
+	speed int = 0
+}
+
+func (car *Car) init(){
+	car.frontLeft_pin = frontLeft_pin
+	car.frontRight_pin = frontRight_pin
+	car.rearLeft_pin = rearLeft_pin
+	car.rearRight_pin = rearRight_pin
+	car.leftPWM_pin = leftPWM_pin
+	car.rightPWM_pin = rightPWM_pin
+}
+
+func (car *Car) start(){
+	connectPiGPIO()
+	initPiGPIO(car.frontLeft_pin,1)
+	initPiGPIO(car.frontRight_pin,1)
+	initPiGPIO(car.rearLeft_pin,1)
+	initPiGPIO(car.rearRight_pin,1)
+	initPiPWM(car.leftPWM_pin,50)
+	initPiPWM(car.rightPWM_pin,50)
+	setPiPWNDutyCyle(car.leftPWM_pin,0)
+	setPiPWNDutyCyle(car.rightPWM_pin,0)
+}
+
+func (car *Car) shutdown(){
+	writePiGPIO(car.frontLeft_pin,0)
+	writePiGPIO(car.frontRight_pin,0)
+	writePiGPIO(car.rearLeft_pin,0)
+	writePiGPIO(car.rearRight_pin,0)
+	writePiGPIO(car.leftPWM_pin,0)
+	writePiGPIO(car.rightPWM_pin,0)
+	disconnectPiGPIO()
+}
+
+func (car *Car) setDirection(direction string){
+	switch direction{
+		case "forward":
+			writePiGPIO(car.frontLeft_pin, true)
+			writePiGPIO(car.rearLeft_pin, false)
+			writePiGPIO(car.frontRight_pin, true)
+			writePiGPIO(car.rearRight_pin, false)
+			car.direction = direction
+
+		case "backward":
+			writePiGPIO(car.frontLeft_pin, false)
+			writePiGPIO(car.rearLeft_pin, true)
+			writePiGPIO(car.frontRight_pin, false)
+			writePiGPIO(car.rearRight_pin, true)
+			car.direction = direction
+
+		case "rotate":
+			writePiGPIO(car.frontLeft_pin, false)
+			writePiGPIO(car.rearLeft_pin, true)
+			writePiGPIO(car.frontRight_pin, true)
+			writePiGPIO(car.rightPWM_pin, false)
+			car.direction = direction
+	}
+}
+
+func (car *Car) setSpeed(speed int){
+	setPiPWNDutyCyle(car.rightPWM_pin, speed)
+	setPiPWNDutyCyle(car.rightPWM_pin, speed)
+}
 
 func connectPiGPIO(){
 	C.pigpio_start(nil,nil)
@@ -19,36 +95,25 @@ func initPiPWM(pin, frequency int){
 func setPiPWNDutyCyle(pin, duty int){
 	C.set_PWM_dutycycle(C.uint(pin), C.uint(duty))
 }
+
+func writePiGPIO(pin int, value bool){
+	C.gpio_write(C.uint(pin),C.uint(value))
+}
+
 func disconnectPiGPIO(){
 	C.pigpio_stop()
 }
 
 
 func main(){
-	var frontLeft_pin int = 17
-	var frontRight_pin int = 22
-	var rearLeft_pin int = 27
-	var rearFront_pin int = 23
-
-	var leftPWM_pin int = 18
-	var rightPWM_pin int = 25
-
-	connectPiGPIO()
-	initPiGPIO(frontLeft_pin,1)
-	initPiGPIO(frontRight_pin,1)
-	initPiGPIO(rearLeft_pin,1)
-	initPiGPIO(rearFront_pin,1)
-	initPiPWM(leftPWM_pin,50)
-	initPiPWM(rightPWM_pin,50)
-
-	setPiPWNDutyCyle(leftPWM_pin, 200)
-	setPiPWNDutyCyle(rightPWM_pin, 200)
+	car := new(Car)
+	car.init()
+	car.start()
+	car.setDirection("forward")
+	car.setSpeed(200)
 	time.Sleep(3 * time.Second)
-	setPiPWNDutyCyle(leftPWM_pin, 100)
-	setPiPWNDutyCyle(rightPWM_pin, 100)
+	car.setDirection("backward")
+	car.setSpeed(100)
 	time.Sleep(5 * time.Second)
-
-	setPiPWNDutyCyle(leftPWM_pin, 0)
-	setPiPWNDutyCyle(rightPWM_pin, 0)
-	disconnectPiGPIO()
+	car.shutdown()
 }
